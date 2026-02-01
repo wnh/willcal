ce3b5055f0373052d0064e0b35fcd463e29273cb
Will Harding <wharding@geologicai.com>
2026-01-31 21:57:30 -0700

    Click and drag to add an item

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
-import { Calendar, dateFnsLocalizer } from 'react-big-calendar';
+import { Calendar, dateFnsLocalizer, SlotInfo } from 'react-big-calendar';
 import format from 'date-fns/format';
 import parse from 'date-fns/parse';
 import startOfWeek from 'date-fns/startOfWeek';
 import getDay from 'date-fns/getDay';
 import enUS from 'date-fns/locale/en-US';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
+
+interface CalendarEvent {
+  id: number;
+  title: string;
+  start: Date;
+  end: Date;
+}
 
 const locales = {
   'en-US': enUS,
@@ -21,12 +28,26 @@ const localizer = dateFnsLocalizer({
 });
 
 function App() {
+  const [events, setEvents] = useState<CalendarEvent[]>([]);
+
+  const handleSelectSlot = (slotInfo: SlotInfo) => {
+    const newEvent: CalendarEvent = {
+      id: Date.now(),
+      title: 'New Event',
+      start: slotInfo.start,
+      end: slotInfo.end,
+    };
+    setEvents([...events, newEvent]);
+  };
+
   return (
     <div style={{ height: '100vh' }}>
       <Calendar
         localizer={localizer}
-        events={[]}
+        events={events}
         defaultView="week"
+        selectable
+        onSelectSlot={handleSelectSlot}
         style={{ height: '100%' }}
       />
     </div>
